e47071ba0893339977c6f936032b308a3849ac8a
Vuyo Mpondo <vuyo.kev.mpondo@gmail.com>
2019-03-18 17:05:30 +0200

    my first package

---
+++ README.md
@@ -0,0 +1,11 @@
+# mypackage
+This library was created as an example on how to publish your own python library
+
+# Building this package locally
+'python.setup.py sdist'
+
+## Installing this package from Github
+'pip install git+ https://github.com/vuyokevin/mypackage'
+
+## Updating this package from Github
+'pip install --upgrade git+ https://github.com/vuyokevin/mypackage'
---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/<vuyokevin>/<mypackage>',
+    author='vuyo',
+    author_email='vuyo.kev.mpondo@gmail.com'
+)      
